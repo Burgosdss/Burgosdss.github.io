9ad77c4838b80497dad5f27387b8f74f85436d42
Diego Burgos <diegoburgos@MacBook-Pro.attlocal.net>
2020-07-16 15:21:08 -0500

    version with notes

---
+++ README.md
@@ -0,0 +1,37 @@
+# The Strain Libray
+
+A web app for gathering and displaying Strain data!; Maybe Selling in the future.
+
+
+# Screenshots
+
+![screenshot 0](https://imgur.com/I4XzX6f.png)
+
+![screenshot 1](https://imgur.com/eYjBYVG.png)
+
+![screenshot 2](https://imgur.com/FX0MxGA.png)
+
+![screenshot 3](https://imgur.com/0NMtkyU.png)
+
+![screenshot 4](https://imgur.com/xStxPOs.png)
+
+![screenshot 5](https://imgur.com/ldYqU48.png)
+
+# Technologies
+
+- HTML
+- CSS
+- Google Fonts
+- Materialize 
+- JavaScript
+- jQuery
+
+# Getting Started
+[Click here]() to open app in browser
+
+# Future Enhancements
+1. A Search feature to allow for users to search an specific strain. 
+2. Showing more detail for each Strain
+3. Larger sprite images and enhanced styling for each strain and effecs. 
+4. Make a shopping option a become a dealer ? just kidding. 
+
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 // Options-to-Search:
 //Search All:
-// https:strainapi.evanbusse.com/JuedK7A/strains/search/all
+// https:strainapi.evanbusse.com/JuedK7A/strains/search/allt
 
 //Search Strains by name:
 // https:strainapi.evanbusse.com/JuedK7A/strains/search/name/NAME
@@ -29,98 +29,109 @@
 
 // http:strainapi.evanbusse.com/JuedK7A/strains/data/desc/STRAIN_ID
 
+$(function(){   
+
 // ----------------- Constants ----------------- 
 // const secondURL = 'http:strainapi.evanbusse.com/JuedK7A/strains/data/desc/STRAIN_ID'
-const baseURL = 'https:strainapi.evanbusse.com/JuedK7A/strains/search/all'
-const $modal = $('.modal');
-const $name = $('#name');
-const $race = $('#race');
-const $flavors = $('#flavors');
-const $peffects = $('#peffects');
-const $neffects = $('#neffects');
-const $medical = $('#medical');
-
-// ----------------- Variables ----------------- 
-var val;
-let strain, strainDetail; 
-
-// ----------------- Elements References -------
-
-const $ulEl = $('.collection');
-
-// ----------------- Event Listener ------------
-
-$ulEl.on('click', 'span', handleClick);
 
 
-// ----------------- functions ------------
+    const baseURL = 'https:strainapi.evanbusse.com/JuedK7A/strains/search/all'
+    const $modal = $('.modal');
+    const $name = $('#name');
+    const $race = $('#race');
+    const $flavors = $('#flavors');
+    const $peffects = $('#peffects');
+    const $neffects = $('#neffects');
+    const $medical = $('#medical');
 
-$modal.modal();
-const instance = M.Modal.getInstance($modal);
+    // ----------------- Variables ----------------- 
+    var val;
+    let strain, strainDetail; 
+
+    // ----------------- Elements References -------
+
+    const $ulEl = $('.collection');
+
+    // ----------------- Event Listener ------------
+
+    $ulEl.on('click', 'span', handleClick);
 
 
-function handleClick(event){
-    let strainKey = this.id.split('-');
-    var strainInfo = strain[parseInt(strainKey[0])][strainKey[1]];
-    // console.log(strainKey);
-    // getStrain(baseURL + event.target, true);
-    $name.text("Name: " + strainKey[1]);
-    $race.text("Race: " + strainInfo.race);
-    $flavors.text("Flavors: " + strainInfo.flavors);
-    $peffects.text("Positive Effects: " + strainInfo.effects.positive);
-    $neffects.text("Negative Effects: " + strainInfo.effects.negative);
-    $medical.text("Common Usage: " + strainInfo.effects.medical);
-    instance.open();
-    // console.log($(this).attr("id"));
-}
-// Make data available
-getStrain(baseURL);
+    // ----------------- functions ------------
 
-function getStrain(detailURL, isDetail = false){
-    
-     detailURL = baseURL
-    $.ajax(detailURL)
-    .then(
-        function(data){
-            console.log("Data: ", data);
-            // console.log("Date effects: ", data.Afpak.effects);
+    $modal.modal();
+    const instance = M.Modal.getInstance($modal);
 
+
+    function handleClick(event){
+        let strainKey = this.id.split('-');
+        var strainInfo = strain[parseInt(strainKey[0])][strainKey[1]];
+        // console.log(strainKey);
+        // getStrain(baseURL + event.target, true);
+        $name.text("Name: " + strainKey[1]);
+        $race.text("Race: " + strainInfo.race);
+        $flavors.text("Flavors: " + strainInfo.flavors);
+        $peffects.text("Positive Effects: " + strainInfo.effects.positive);
+        $neffects.text("Negative Effects: " + strainInfo.effects.negative);
+        $medical.text("Common Usage: " + strainInfo.effects.medical);
+        instance.open();
+        // console.log($(this).attr("id"));
+    }
+    // Make data available
+    getStrain(baseURL);
+
+    function getStrain(detailURL, isDetail = false){
+        
+        detailURL = baseURL
+        $.ajax(detailURL)
+        .then(
+            function(data){
+                // console.log("Data: ", data);
+                // console.log("Date effects: ", data.Afpak.effects);
+
+                
+
+                if(!isDetail){
+                    strain = Object.entries(data).map(function(s){
+                        return {[s[0]]:s[1]}
+                        }).slice(0, 15)
+                                // console.log("Strain: ", Object.keys(strain[0]));
+                                // console.log("Data effects:", strain[1]);
+                render();
+
+                } else {
             
+                render(true, s);
+                }
+            
+                val = Object.values(strain[0]);
+                // console.log("Data values:", Object.values(strain[0]));
+                // console.log("Data var:", val[0].flavors);
+        },     function(error){
+                    console.log("Error: ", error);
+        });
+    }
 
-            if(!isDetail){
-                strain = Object.entries(data).map(function(s){
-                    return {[s[0]]:s[1]}
-                    }).slice(0,51)
-                            // console.log("Strain: ", Object.keys(strain[0]));
-                            // console.log("Data effects:", strain[1]);
-            render();
 
-            } else {
-           
-            render(true, s);
-            }
-           
-            val = Object.values(strain[0]);
-            console.log("Data values:", Object.values(strain[0]));
-            console.log("Data var:", val[0].flavors);
-    },     function(error){
-                console.log("Error: ", error);
+    function generateHTML(){
+        return strain.forEach(function(s, index){
+            // console.log(s);
+            $ulEl.append(`<li class="collection-item"><div>${Object.keys(s)[0, 0]}<span class="secondary-content" id="${index}-${Object.keys(s)[0, 0]}">Detail</div></li>`) 
+        });
+    }
+
+    function render(isDetail = false) {
+        if(!isDetail){
+            const html = generateHTML();
+            $ulEl.html(html)
+        }    
+    }
+
+    $(document).ready(function(){
+        $('.tap-target').tapTarget();
+
+            $('.tap-button').on('click', function(){
+                $('.tap-target').tapTarget('open')
+    })
     });
-}
-
-
-function generateHTML(){
-    return strain.forEach(function(s, index){
-        // console.log(s);
-        $ulEl.append(`<li class="collection-item"><div>${Object.keys(s)[0, 0]}<span class="secondary-content" id="${index}-${Object.keys(s)[0, 0]}">Detail</div></li>`) 
-    });
-}
-
-function render(isDetail = false) {
-    if(!isDetail){
-        const html = generateHTML();
-        $ulEl.html(html)
-    }    
-}
-
-
+});
